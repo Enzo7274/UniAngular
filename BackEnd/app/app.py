4c93fdb44b0a946945241f35d24fafc530e2767a
Enzo7274 <enzobahia2010@hotmail.com>
2023-06-18 21:26:24 -0300

    Sala creation update

--- BackEnd/app/app.py
+++ BackEnd/app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.modules.usuario.usuario_routes import usuario_routes
 from app.modules.disciplina.disciplina_routes import disciplina_routes
+from app.modules.sala.sala_routes import sala_routes
 from app.modules.curso.curso_routes import curso_routes
 from app.modules.login.login_routes import login_routes
 
@@ -10,4 +11,5 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 app.register_blueprint(usuario_routes)
 app.register_blueprint(curso_routes)
 app.register_blueprint(disciplina_routes)
+app.register_blueprint(sala_routes)
 app.register_blueprint(login_routes)
